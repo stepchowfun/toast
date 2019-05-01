8935cc9ac393fe8e14331f8270e75efd597287d2
Stephan Boyer <stephan@stephanboyer.com>
2019-05-01 01:16:17 -0700

    Install v0.2.0 by default

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@
   DESTINATION="${PREFIX:-/usr/local/bin}/bake"
 
   # Which version to download
-  RELEASE="v${VERSION:-0.1.0}"
+  RELEASE="v${VERSION:-0.2.0}"
 
   # Determine which binary to download.
   FILENAME=''
