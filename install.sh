0837d68d62ef0e236c4fa3b33d669420024b2196
Stephan Boyer <stephan@stephanboyer.com>
2019-05-18 22:58:46 -0700

    Install v0.12.0 by default and use that version for CI

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@
   DESTINATION="${PREFIX:-/usr/local/bin}/bake"
 
   # Which version to download
-  RELEASE="v${VERSION:-0.11.0}"
+  RELEASE="v${VERSION:-0.12.0}"
 
   # Determine which binary to download.
   FILENAME=''
