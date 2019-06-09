d1a1021788f509d9d7418549bbe21222399652a7
Stephan Boyer <stephan@stephanboyer.com>
2019-06-09 04:30:56 -0700

    Fix a bug in the way symlinks in `output_paths` are handled

---
+++ integration-tests/output-paths/nested-symlink/run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+"$TOAST" --read-local-cache false --write-local-cache false
+
+# shellcheck disable=SC2010
+ls -al | grep '^drwxr.xr.x.* foo'
+
+# shellcheck disable=SC2010
+ls -al foo | grep '^lrwxr.xr.x.* symlink \-> non\-existent'
+
+rm -rf foo
