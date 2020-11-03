173ef0428364e151238eda640188a3cda8de24d4
Stephan Boyer <stephan@stephanboyer.com>
2020-11-03 13:34:23 -0800

    Introduce a patch for CVE-2020-15228

--- .github/actions/toast/index.js
+++ .github/actions/toast/index.js
@@ -1,6 +1,7 @@
 const childProcess = require('child_process');
 const core = require('@actions/core');
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
 // The Toast version
 const toastVersion = '0.32.0';
@@ -20,6 +21,14 @@ const writeRemoteCache = writeRemoteCacheInput == 'true';
 // Where to install Toast.
 const toastPrefix = process.env.HOME;
 
+// Before doing anything, disable command workflow processing. This is to prevent an injection
+// attack with GitHub Actions. For more information, see:
+// - https://bugs.chromium.org/p/project-zero/issues/detail?id=2070&can=2&q=&colspec=ID%20Type%20Status%20Priority%20Milestone%20Owner%20Summary&cells=ids
+// - https://github.blog/changelog/2020-10-01-github-actions-deprecating-set-env-and-add-path-commands/
+// - https://github.com/actions/toolkit/security/advisories/GHSA-mfwh-5m23-j46w
+const token = uuidv4();
+console.log(`::stop-commands::${token}`);
+
 // Install Toast.
 childProcess.execSync(
   'curl https://raw.githubusercontent.com/stepchowfun/toast/master/install.sh -LSfs | ' +
@@ -31,8 +40,11 @@ childProcess.execSync(
 const taskArgs = tasks === null ? [] : tasks;
 const fileArgs = file === null ? [] : ['--file', file];
 const repositoryArgs = repo === null ? [] : ['--repo', repo];
-const readRemoteCacheArgs = repo === null ? [] : ['--read-remote-cache', 'true'];
-const writeRemoteCacheArgs = writeRemoteCache ? ['--write-remote-cache', 'true'] : [];
+const readRemoteCacheArgs =
+  repo === null ? [] : ['--read-remote-cache', 'true'];
+const writeRemoteCacheArgs = writeRemoteCache
+  ? ['--write-remote-cache', 'true']
+  : [];
 
 // Run Toast.
 childProcess.execFileSync(
@@ -47,3 +59,6 @@ childProcess.execFileSync(
     stdio: 'inherit',
   },
 );
+
+// To be a good citizen, we now re-enable command workflow processing.
+console.log(`::${token}::`);
