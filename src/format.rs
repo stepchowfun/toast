e03a5a32518a5820f35ccde80f2b86d2dd389347
Stephan Boyer <stephan@stephanboyer.com>
2021-04-08 17:34:51 -0700

    Add tests to `format.rs`

--- src/format.rs
+++ src/format.rs
@@ -1,9 +1,8 @@
 use colored::{control::SHOULD_COLORIZE, ColoredString, Colorize};
 
-// This trait has a function for formatting "code-like" text, such as a task name or a file path.
-// The reason it's implemented as a trait and not just a function is so we can use it with method
-// syntax, as in `x.code_str()`. Rust does not allow us to implement methods on primitive types
-// such as `str`.
+// This trait has a function for formatting "code-like" text, such as a file path. The reason it's
+// implemented as a trait and not just a function is so we can use it with method syntax, as in
+// `x.code_str()`. Rust does not allow us to implement methods on primitive types such as `str`.
 pub trait CodeStr {
     fn code_str(&self) -> ColoredString;
 }
@@ -47,7 +46,24 @@ pub fn series(items: &[String]) -> String {
 
 #[cfg(test)]
 mod tests {
-    use crate::format::{number, series};
+    use crate::format::{number, series, CodeStr};
+
+    #[test]
+    fn code_str_colorize_off_display() {
+        // This test, like many others, depends on colors being disabled [ref:colorless_tests].
+        assert_eq!(format!("{}", "foo".code_str()), "`foo`");
+    }
+
+    #[test]
+    fn code_str_colorize_on_display() {
+        // If tests are run in parallel, then this may interfere with other tests. So we are forced
+        // to run tests sequentially [ref:sequential_tests].
+        colored::control::set_override(true);
+
+        assert_eq!(format!("{}", "foo".code_str()), "\u{1b}[35mfoo\u{1b}[0m");
+
+        colored::control::unset_override();
+    }
 
     #[test]
     fn number_zero() {
