6e683c1d4ef274f1aac96df10d8caba5d7011faf
Stephan Boyer <stephan@stephanboyer.com>
2021-11-15 05:14:17 -0800

    Install v0.45.2 by default

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@
   DESTINATION="${PREFIX:-/usr/local/bin}/toast"
 
   # Which version to download
-  RELEASE="v${VERSION:-0.45.1}"
+  RELEASE="v${VERSION:-0.45.2}"
 
   # Determine which binary to download.
   FILENAME=''
