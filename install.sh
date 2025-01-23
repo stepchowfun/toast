7e4c112f594b45fcca64a17266f6043e5420315a
Stephan Boyer <stephan@stephanboyer.com>
2025-01-22 20:18:34 -0800

    Release v0.47.7

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@
   DESTINATION="${PREFIX:-/usr/local/bin}/toast"
 
   # Which version to download
-  RELEASE="v${VERSION:-0.47.6}"
+  RELEASE="v${VERSION:-0.47.7}"
 
   # Determine which binary to download.
   FILENAME=''
