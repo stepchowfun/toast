13407c73ba7618ee65737052081c426ae4ce0514
Stephan Boyer <stephan@stephanboyer.com>
2025-03-20 02:03:12 -0700

    Update the MAINTAINERS instructions

--- integration-tests/base-images/centos/greet.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -euxo pipefail
-
-echo 'Hello, World!'
--- integration-tests/base-images/centos/run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euxo pipefail
-
-"$TOAST" --read-local-cache false --write-local-cache false
-grep Hello output.txt
-rm output.txt
